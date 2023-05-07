8f30632e02153060cd28691787fe1207cce2fea8
Akas_Datta <012akasdatta@gmail.com>
2023-05-08 01:05:59 +0600

    added react google map and component myLocation here and some file modified

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import MyLocation from './components/MyLocation';
+const App = () => {
+  return (
+    <div className='App'>
+      <MyLocation></MyLocation>
+    </div>
+  );
+};
+
+export default App;
